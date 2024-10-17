338d5d6dcfc37f9325ddac56e98ce34810a8ebc0
Kinjal Jain <jainkinjal021196@gmail.com>
2024-10-17 19:15:14 +0530

    Sign in screen completed

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,23 +2,32 @@ import { View, Text , Image, ScrollView} from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {images} from '../../constants';
+import { Link } from 'expo-router';
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password:""
   })
+
+  const[isSubmit , setIsSubmit] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full"
     >
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[83vh] px-4 my-6">
           <Image 
             source={images.logo}
-            resizeMode='contain' className="w-[115px] h-[35px]"
+            resizeMode='contain' className="w-[115px] h-[34px]"
           />
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+          <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">Log in to Aora</Text>
           <FormField 
             title="Email"
             value={form.email}
@@ -37,6 +46,18 @@ const SignIn = () => {
             otherStyles="mt-7"
             
           />
+          <CustomButton 
+            title= "Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmit}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
