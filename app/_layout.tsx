ed68af19e6645ce779e1744eecc165982e499620
Kinjal Jain <jainkinjal021196@gmail.com>
2024-10-21 19:57:54 +0530

    Wrapped the app in GlovalProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Slot, SplashScreen, Stack } from 'expo-router';
 import React, { useEffect } from 'react';
 import { useFonts } from 'expo-font';
+import {GlobalProvider} from '../context/GlobalProvider';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,7 +31,8 @@ const RootLayout = () => {
     return null;
   }
   return (
-    <Stack>
+    <GlobalProvider>
+          <Stack>
       <Stack.Screen name="index" options={{headerShown : false }} />
       <Stack.Screen name="(auth)" options={{headerShown : false }} />
       <Stack.Screen name="(tabs)" options={{headerShown : false }} />
@@ -39,6 +41,8 @@ const RootLayout = () => {
 
 
     </Stack>
+    </GlobalProvider>
+
 )
 }
 
