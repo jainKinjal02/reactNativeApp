cf26c61ad9105615308dfd02568de548c9d965bc
Kinjal Jain <jainkinjal021196@gmail.com>
2024-11-05 21:52:54 +0530

    Added stylign to home screen and images too

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,15 +1,16 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {images} from '../../constants';
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary">
       <FlatList 
         data={[{ id: 1 }, { id: 2 }]}
         keyExtractor={(item)=> item.$id}
         renderItem={({item}) => (
-          <Text className="text-3xl">
+          <Text className="text-3xl text-white">
             {item.id}
           </Text>
         )}
@@ -17,12 +18,17 @@ const Home = () => {
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
                 <View>
-                  <Text>
+                  <Text className="font-pmedium text-sm text-gray-100">
                     Welcome!
                   </Text>
-                  <Text>
+                  <Text className="text-2xl font-psemibold text-white">
                     Kinjal
                   </Text>
+                </View>
+                <View className="mt-1.5">
+                  <Image source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode='contain' />
                 </View>
             </View>
           </View>
